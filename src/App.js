1988d5fd0539c3fa26e05117618d5bda9b882aa5
Edson Junior <edson.junior@mereo.com>
2024-01-09 16:38:53 -0300

    Enviando dados via props

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Post from './components/Post';
 function App() {
   return (
     <main>
-      <Post />
+      <Post author ='Edson Junior' body='React.js' />
+      <Post author ='Paula Pinheiro' body='React.js 2' />
     </main>
   );
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,10 @@
-const names = ['Edson Jr' , 'Paula Pinheiro'];
-const chosenName = Math.random() > 0.5 ? names[0] : names[1];
 
-function Post(){
+function Post(props){
 
     return (
         <div>
-            <p>React.js</p>
-            <p>{ chosenName }</p>
+            <p>{ props.body }</p>
+            <p>{ props.author }</p>
         </div>
     );
 }
