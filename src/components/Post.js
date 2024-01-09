a009e5f4701f369359e0bd143de81df3229a634f
Edson Junior <edson.junior@mereo.com>
2024-01-09 17:03:54 -0300

    Adicionando classes css ao componente Post

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,23 +2,9 @@ import classes from './Post.module.css';
 
 function Post(props){
     return (
-        <div>
-            <div style={{color : "red"}}>
-                <p>Css inline junto do HTML</p>
-            </div>
-
-            <div className="post">
-                <p>Usando Css arquivo GLOBAL</p>
-                <p>{ props.body }</p>
-                <p>{ props.author }</p>
-            </div>
-
-            <div className={classes.post2}>
-                <p>Usando Css arquivo </p>
-                <p>{ props.body }</p>
-                <p>{ props.author }</p>
-            </div>
-           
+        <div className={classes.post}>
+            <p className={classes.author}>{ props.author }</p>
+            <p className={classes.text}>{ props.body }</p>
         </div>
     );
 }
