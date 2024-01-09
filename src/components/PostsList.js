857db84defab233500ce1ff06081c58f356cbdd5
Edson Junior <edson.junior@mereo.com>
2024-01-09 17:41:07 -0300

    Criando o componente NewPost para trabalhar com eventos

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -1,13 +1,17 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 
 function PostsList(){
 
     return (
-       <ul className={classes.posts}>
-            <Post author ='Edson Junior' body='React.js' />
-            <Post author ='Paula Pinheiro' body='React.js 2' />
-       </ul>
+        <>
+            <NewPost />
+            <ul className={classes.posts}>
+                <Post author ='Edson Junior' body='React.js' />
+                <Post author ='Paula Pinheiro' body='React.js 2' />
+            </ul>
+        </>  
     );
 }
 export default PostsList;
