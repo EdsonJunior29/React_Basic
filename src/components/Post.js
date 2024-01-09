323e0195790570bfc477d40cb64c28a7818be665
Edson Junior <edson.junior@mereo.com>
2024-01-09 16:55:15 -0300

    css
    Utilizando css inline, de forma global e exclusivo para o componente.

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,10 +1,24 @@
+import classes from './Post.module.css';
 
 function Post(props){
-
     return (
         <div>
-            <p>{ props.body }</p>
-            <p>{ props.author }</p>
+            <div style={{color : "red"}}>
+                <p>Css inline junto do HTML</p>
+            </div>
+
+            <div className="post">
+                <p>Usando Css arquivo GLOBAL</p>
+                <p>{ props.body }</p>
+                <p>{ props.author }</p>
+            </div>
+
+            <div className={classes.post2}>
+                <p>Usando Css arquivo </p>
+                <p>{ props.body }</p>
+                <p>{ props.author }</p>
+            </div>
+           
         </div>
     );
 }
