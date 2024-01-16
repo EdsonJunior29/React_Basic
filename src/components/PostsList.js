9c0a288d20368e8c2d57edb79937b8322ccbc074
Edson Junior <edson.junior@mereo.com>
2024-01-16 13:16:50 -0300

    Adiciona loading antes de carregar as informações

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -6,12 +6,15 @@ import Modal from './Modal';
 
 function PostsList({ isPosting, onStopPosting }){
     const [posts, setPosts] = useState([]);
+    const [isFetching, setIsFetching] = useState(false);
 
     useEffect(() => {
         async function fetchPosts() {
-            const response = await fetch('http://localhost:3001/posts')
+            setIsFetching(true);
+            const response = await fetch('http://localhost:3001/posts');
             const resData = await response.json();
             setPosts(resData);
+            setIsFetching(false);
         }
 
         fetchPosts();
@@ -39,18 +42,24 @@ function PostsList({ isPosting, onStopPosting }){
                 </Modal>
             )}
 
-            {posts.length > 0 && (
+            {!isFetching && posts.length > 0 && (
                  <ul className={classes.posts}>
                     {posts.map((post) => <Post key={post.body} author ={post.author} body={post.body}/>)}
                  </ul>
             )}
 
-            {posts.length === 0 && 
+            {!isFetching && posts.length === 0 && 
                 <div style={{textAlign: 'center', color: 'black'}}>
                     <h2>There are no posts yet.</h2>
                     <p>Start adding some!</p>
                 </div>
             
+            }
+
+            {isFetching && 
+                <div style={{textAlign: 'center', color:'white'}}>
+                    <p>Loading posts ..........</p>
+                </div>
             }
         </>  
     );
