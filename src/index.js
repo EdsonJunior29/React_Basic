a272504d8260b4d26a649d34b0d967e3d882288d
Edson Junior <edson.junior@mereo.com>
2024-01-16 13:57:34 -0300

    Criando a rota principal e as rotas filhas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import NewPost from './components/NewPost';
+import RootLayout from './routes/RootLayout';
 
 const router = createBrowserRouter([
-  {path: '/' , element: <App />}
+  {path: '/' , element: <RootLayout /> ,
+    children: [
+      {path: '/' , element: <App />},
+      {path: '/create-post' , element: <NewPost />}
+    ]
+  },
+ 
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
