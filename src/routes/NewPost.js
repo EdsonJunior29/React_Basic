55b6e67b94f7642638e1283771631086b6b2b49a
Edson Junior <edson.junior@mereo.com>
2024-01-16 21:07:35 -0300

    Utilizando o Hook Navigate

--- src/routes/NewPost.js
+++ src/routes/NewPost.js
@@ -1,9 +1,10 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import Modal from '../components/Modal';
 
 import classes from '../components/NewPost.module.css';
 
-function NewPost({onCancel,  onAddPost}) {
+function NewPost({ onAddPost}) {
 
     const [enteredBody , setEnteredBody] = useState('');
     const [enteredAuthor , setEnteredAuthor] = useState('');
@@ -23,7 +24,6 @@ function NewPost({onCancel,  onAddPost}) {
             body : enteredBody,
         };
         onAddPost(postData);
-        onCancel();
     }
 
     return (
@@ -38,7 +38,7 @@ function NewPost({onCancel,  onAddPost}) {
                     <input type="text" id="name" required onChange={changeAuthorHandler}/>
                 </p>
                 <p className={classes.actions}>
-                    <button type='button' onClick={onCancel}>Cancel</button>
+                    <Link type='button' to={'..'} >Cancel</Link>
                     <button>Submit</button>
                 </p>
             </form>
